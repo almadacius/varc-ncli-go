5a0578292923a6b76a88e79761cc5bba674d9de5
Vinicius Almada - HOME <almadacius@outlook.com>
2023-09-06 21:09:15 -0300

    feat: add file lock mechanics for robustness

--- _tests-integration/tests/file-lock.sh
+++ _tests-integration/tests/file-lock.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
+function writeWave () {
+  # simultaneous writes to the same file
+  # should NOT corrupt
+  varcgo set "far.bring" "new-val" &
+  varcgo set "far.it" "incredible" &
+
+  varcgo get "far.bring" > /dev/null &
+  varcgo get "far.it" > /dev/null &
+  varcgo get "far.on" > /dev/null &
+
+  varcgo set "far.on" "nice" &
+  varcgo set "far.again" "again" &
+  varcgo set "far.andAgain" "other-value" &
+  varcgo set "far.again" "again2" &
+  varcgo set "far.key3" "again2" &
+  varcgo set "far.key4" "again2" &
+  varcgo set "far.key5" "again2" &
+}
+
 function runtest () {
   local HERE=$(cd $(dirname $BASH_SOURCE) && pwd -P)
 
@@ -15,11 +34,14 @@ function runtest () {
   # clear scope
   varcgo del "far"
 
-  # simultaneous writes to the same file
-  # should NOT corrupt
-  varcgo set "far.bring" "new-val" &
-  varcgo set "far.it" "incredible" &
-  varcgo set "far.on" "nice" &
+  writeWave
+  writeWave
+  writeWave
+  writeWave
+  writeWave
+  writeWave
+  writeWave
+  writeWave
 
   # ===================================
   local out=$(varcgo get "far.bring")
--- src/utils/jsonfile/jsonfile.go
+++ src/utils/jsonfile/jsonfile.go
@@ -9,19 +9,59 @@ import (
 // ================================================
 func Save(path string, data map[string]string) {
   jsonBytes, err := json.Marshal(data)
-  utils.LogError(err)
-
-  err = os.WriteFile(path, jsonBytes, 0644)
   utils.LogErrorAndPanic(err)
+
+  writeFileWithLock(path, jsonBytes, 0644)
+}
+
+func Reset(path string) map[string] string {
+  emptyData := emptyMap()
+  Save(path, emptyData)
+  return emptyData
 }
 
 func Load(path string) map[string]string {
-  jsonBytes, err := os.ReadFile(path)
-  utils.LogErrorAndPanic(err)
+  jsonBytes := readFileWithLock(path)
 
   var data map[string] string
 
-  err = json.Unmarshal(jsonBytes, &data)
+  err := json.Unmarshal(jsonBytes, &data)
+  utils.LogError(err)
+  // file is broken, reset it for now
+  if err != nil {
+    data = Reset(path)
+  }
+
+  return data
+}
+
+// ================================================
+func emptyMap() map[string] string {
+  out := make(map[string] string)
+  return out
+}
+
+func writeFileWithLock(path string, data []byte, mode os.FileMode) {
+  file := CreateFile(path)
+  file.OpenWrite(mode)
+  defer file.Close()
+  file.Lock()
+  defer file.Unlock()
+
+  _, err := file.Write(data)
+  utils.LogErrorAndPanic(err)
+}
+
+func readFileWithLock(path string) []byte {
+  file := CreateFile(path)
+  file.OpenRead()
+  defer file.Close()
+  file.Lock()
+  defer file.Unlock()
+
+  // not really sure this is respecting the lock
+  // still getting some eventual read errors on the tests
+  data, err := os.ReadFile(path)
   utils.LogErrorAndPanic(err)
 
   return data
